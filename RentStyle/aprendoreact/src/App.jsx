8e83df23868dbbca84c7f1309446cd8f6b6a67c2
fabian485 <703jrj1997@gmail.com>
2026-06-01 16:47:15 -0500

    Plantilla

--- RentStyle/aprendoreact/src/App.jsx
+++ RentStyle/aprendoreact/src/App.jsx
@@ -1,9 +1,11 @@
-import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Inicio from './pages/Inicio';
+
 
 function App() {
   return (
     <>
-    <h1>RentStyle</h1>
+    <Inicio />
     </>
   )
 }
---
+++ RentStyle/aprendoreact/src/pages/Inicio.jsx
@@ -0,0 +1,8 @@
+function Inicio() {
+    return (
+        <div>
+            <h2>Bienvenido a RentStyle!</h2>
+        </div>
+    );
+}
+export default Inicio;
